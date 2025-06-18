d57b5307f2a347984edc0e7c4cc4cdf8269f0105
anusha2211 <anushaagrawal67@gmail.com>
2025-06-18 11:52:42 -0400

    Initialize React frontend with basic cleanup

---
+++ client/src/App.js
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div>
+      <h1>Welcome to Tactix</h1>
+      <p>This is your Agile sprint planning and task management app.</p>
+    </div>
+  );
+}
+
+export default App;
